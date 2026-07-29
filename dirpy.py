b2742033940d72c7150e2ba2ed619649b6b3bf99
lucasweber20 <lucasweber449@gmail.com>
2026-07-29 14:46:08 -0300

    Feat: add arguments flags

--- dirpy.py
+++ dirpy.py
@@ -1,4 +1,14 @@
+import argparse
 
+
+parser = argparse.ArgumentParser()
+
+args = parser.add_argument("-u", "--url", help='Set url, example: -u https://example.com/', type=str)
+args = parser.add_argument("-w", "--wordlist", help="Specify file with values, example: -o directories.txt", type=str)
+args = parser.add_argument("-o", "--output", help="Specify output file, example: -o outputs.txt", type=str)
+args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 3", type=int)
+
+args = parser.parse_args()
 
 def main():
     pass
