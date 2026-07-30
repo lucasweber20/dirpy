c1584ada3fd45fb3588c2f06c9a1bae8373f4bfb
lucasweber20 <lucasweber449@gmail.com>
2026-07-30 12:36:33 -0300

    Refactor: print output

--- dirpy.py
+++ dirpy.py
@@ -11,7 +11,6 @@ args = parser.add_argument("-u", "--url", help='Set url, example: -u https://exa
 args = parser.add_argument("-w", "--wordlist", help="Specify file with values, example: -o directories.txt", type=str)
 args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 5", default=1, type=int)
 args = parser.add_argument("-sc", "--status_code", help="Specify status code, example: -sc 200 or -sc 200,301", type=str)
-args = parser.add_argument("-nsc", "--no_status_code", help="Specify status code rejected, example: -nsc 404 or -nsc 404,403", type=str)
 args = parser.add_argument("-to", "--timeout", help="Specify timeout in requests, example: --timeout 10", default=5, type=int)
 args = parser.add_argument("-d", "--delay", help="Specify delay in requests, example: --delay 5", type=int)
 args = parser.add_argument("-o", "--output", help="Specify output file, example: -o outputs.txt", type=str)
@@ -23,7 +22,6 @@ def main():
     url = args.url
     wordlist = args.wordlist
     status_code = args.status_code
-    no_status_code = args.no_status_code
     threads = args.thread
     output = args.output
     timeout = args.timeout
@@ -43,13 +41,13 @@ def main():
                 time.sleep(delay)
             if status_code_result:
                 if status_code_result[0] >= 200 and status_code_result[0] < 300:
-                    print(f"{status_code_result[1]}: \033[92m{status_code_result[0]}\033[00m")
+                    print(f"{status_code_result[1]} -> \033[92m{status_code_result[0]}\033[00m")
                 elif status_code_result[0] >= 300 and status_code_result[0] < 400:
-                    print(f"{status_code_result[1]}: \033[36m{status_code_result[0]}\033[00m")
+                    print(f"{status_code_result[1]} -> \033[36m{status_code_result[0]}\033[00m")
                 elif status_code_result[0] >= 400 and status_code_result[0] < 500:
-                    print(f"{status_code_result[1]}: \033[33m{status_code_result[0]}\033[00m")
+                    print(f"{status_code_result[1]} -> \033[33m{status_code_result[0]}\033[00m")
                 elif status_code_result[0] >= 500 and status_code_result[0] < 600:
-                    print(f"{status_code_result[1]}: \033[31m{status_code_result[0]}\033[00m")
+                    print(f"{status_code_result[1]} -> \033[31m{status_code_result[0]}\033[00m")
                 if output:
                     write_file = open(output, "a").write(f"{status_code_result[1]} -> {status_code_result[0]}\n")
             else:
