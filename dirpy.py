4507e1beaf1d3b433bb9229d8ecce960b64e314d
lucasweber20 <lucasweber449@gmail.com>
2026-07-30 13:02:28 -0300

    Refactor: add extension argument flag

--- dirpy.py
+++ dirpy.py
@@ -11,6 +11,7 @@ args = parser.add_argument("-u", "--url", help='Set url, example: -u https://exa
 args = parser.add_argument("-w", "--wordlist", help="Specify file with values, example: -o directories.txt", type=str)
 args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 5", default=1, type=int)
 args = parser.add_argument("-sc", "--status_code", help="Specify status code, example: -sc 200 or -sc 200,301", type=str)
+args = parser.add_argument("-x", "--extension", help="Specify extension, example: -x .php or -x .php,.js", type=str)
 args = parser.add_argument("-to", "--timeout", help="Specify timeout in requests, example: --timeout 10", default=5, type=int)
 args = parser.add_argument("-d", "--delay", help="Specify delay in requests, example: --delay 5", type=int)
 args = parser.add_argument("-o", "--output", help="Specify output file, example: -o outputs.txt", type=str)
@@ -22,6 +23,7 @@ def main():
     url = args.url
     wordlist = args.wordlist
     status_code = args.status_code
+    extension = args.extension
     threads = args.thread
     output = args.output
     timeout = args.timeout
